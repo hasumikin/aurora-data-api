c665912d5611d8c021370070e239fcd046b8ecdd
HASUMI Hitoshi <hasumikin@gmail.com>
2022-06-03 15:47:05 +0900

    Fix type/boolean

--- lib/aurora-data-api.rb
+++ lib/aurora-data-api.rb
@@ -4,6 +4,7 @@ require_relative "aurora-data-api/version"
 require_relative "aurora-data-api/model"
 require_relative "aurora-data-api/depot"
 require_relative "aurora-data-api/data_service"
+require_relative "aurora-data-api/type/boolean"
 
 module AuroraDataApi
   class Error < StandardError; end
